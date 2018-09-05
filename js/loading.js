7b5eb8aa9d6d2689a988534ba1ee426b8f68df71
Geehoon <junggeehoon@gmail.com>
2018-09-05 14:12:47 +0900

    practice

--- js/loading.js
+++ js/loading.js
@@ -1,18 +1,18 @@
 const onReady = (callback) => {
     var intervalId = window.setInterval(() => {
         if (document.getElementsByTagName('body')[0] !== undefined) {
-          window.clearInterval(intervalId);
-          callback.call(this);
+            window.clearInterval(intervalId);
+            callback.call(this);
         }
-    }, 1000);    
+    }, 1000);
 }
 
-  
+
 const setVisible = (selector, visible) => {
     document.querySelector(selector).style.display = visible ? 'block' : 'none';
 }
 
-  
+
 onReady(() => {
     setVisible('.page', true);
     setVisible('#loading', false);
