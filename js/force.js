0bba7a3866e0c98b3b61ec73e8b5a273b3ba24f4
Geehoon <junggeehoon@gmail.com>
2018-09-05 15:38:17 +0900

    indent

--- js/force.js
+++ js/force.js
@@ -1,104 +1,104 @@
 const fetchData = async () => {
-    const data = await d3.json('http://52.78.57.243:5000/star');
-    console.log(data);
+  const data = await d3.json('http://52.78.57.243:5000/star');
+  console.log(data);
 
-    var svg = d3.select("svg"),
-        width = +svg.attr("width"),
-        height = +svg.attr("height");
+  var svg = d3.select("svg"),
+    width = +svg.attr("width"),
+    height = +svg.attr("height");
 
-    var nodes_data = data
-    // [
-    //     {"name": "Travis", "sex": "M"},
-    //     {"name": "Rake", "sex": "M"},
-    //     {"name": "Diana", "sex": "F"},
-    //     {"name": "Rachel", "sex": "F"},
-    //     {"name": "Shawn", "sex": "M"},
-    //     {"name": "Emerald", "sex": "F"}
-    // ]
-    
-    var simulation = d3.forceSimulation()
-        .nodes(nodes_data);
+  var nodes_data = data
+  // [
+  //     {"name": "Travis", "sex": "M"},
+  //     {"name": "Rake", "sex": "M"},
+  //     {"name": "Diana", "sex": "F"},
+  //     {"name": "Rachel", "sex": "F"},
+  //     {"name": "Shawn", "sex": "M"},
+  //     {"name": "Emerald", "sex": "F"}
+  // ]
 
-
-    //add forces
-    //we're going to add a charge to each node 
-    //also going to add a centering force
-    simulation
-        .force("charge_force", d3.forceManyBody())
-        .force("center_force", d3.forceCenter(width / 2, height / 2));
-
-    //draw circles for the nodes 
-    var node = svg.append("g")
-        .attr("class", "nodes")
-        .selectAll("circle")
-        .data(nodes_data)
-        .enter()
-        .append("circle")
-        .attr("r", 5)
-        .attr("fill", "red");
+  var simulation = d3.forceSimulation()
+    .nodes(nodes_data);
 
 
-    function tickActions() {
-        //update circle positions to reflect node updates on each tick of the simulation 
-        node
-            .attr("cx", function (d) {
-                return d.x;
-            })
-            .attr("cy", function (d) {
-                return d.y;
-            })
+  //add forces
+  //we're going to add a charge to each node 
+  //also going to add a centering force
+  simulation
+    .force("charge_force", d3.forceManyBody())
+    .force("center_force", d3.forceCenter(width / 2, height / 2));
 
-        //update link positions 
-        //simply tells one end of the line to follow one node around
-        //and the other end of the line to follow the other node around
-
-        link
-            .attr("x1", function (d) {
-                return d.source.x;
-            })
-            .attr("y1", function (d) {
-                return d.source.y;
-            })
-            .attr("x2", function (d) {
-                return d.target.x;
-            })
-            .attr("y2", function (d) {
-                return d.target.y;
-            });
-
-    }
-
-    simulation.on("tick", tickActions);
-
-    //Create links data 
-    var links_data = [{
-            "source": 1,
-            "target": 2
-        },
-        // {"source": "Diana", "target": "Rake"},
-        // {"source": "Diana", "target": "Rachel"},
-        // {"source": "Rachel", "target": "Rake"},
-        // {"source": "Rachel", "target": "Shawn"},
-        // {"source": "Emerald", "target": "Rachel"}
-    ]
+  //draw circles for the nodes 
+  var node = svg.append("g")
+    .attr("class", "nodes")
+    .selectAll("circle")
+    .data(nodes_data)
+    .enter()
+    .append("circle")
+    .attr("r", 5)
+    .attr("fill", "red");
 
 
-    //Create the link force 
-    //We need the id accessor to use named sources and targets 
-    var link_force = d3.forceLink(links_data)
-        .id(function (d) {
-            return d.id;
-        })
+  function tickActions() {
+    //update circle positions to reflect node updates on each tick of the simulation 
+    node
+      .attr("cx", function (d) {
+        return d.x;
+      })
+      .attr("cy", function (d) {
+        return d.y;
+      })
 
-    simulation.force("links", link_force)
+    //update link positions 
+    //simply tells one end of the line to follow one node around
+    //and the other end of the line to follow the other node around
 
-    //draw lines for the links 
-    var link = svg.append("g")
-        .attr("class", "links")
-        .selectAll("line")
-        .data(links_data)
-        .enter().append("line")
-        .attr('stroke', 'white')
-        .attr("stroke-width", 2);
+    link
+      .attr("x1", function (d) {
+        return d.source.x;
+      })
+      .attr("y1", function (d) {
+        return d.source.y;
+      })
+      .attr("x2", function (d) {
+        return d.target.x;
+      })
+      .attr("y2", function (d) {
+        return d.target.y;
+      });
+
+  }
+
+  simulation.on("tick", tickActions);
+
+  //Create links data 
+  var links_data = [{
+      "source": 1,
+      "target": 2
+    },
+    // {"source": "Diana", "target": "Rake"},
+    // {"source": "Diana", "target": "Rachel"},
+    // {"source": "Rachel", "target": "Rake"},
+    // {"source": "Rachel", "target": "Shawn"},
+    // {"source": "Emerald", "target": "Rachel"}
+  ]
+
+
+  //Create the link force 
+  //We need the id accessor to use named sources and targets 
+  var link_force = d3.forceLink(links_data)
+    .id(function (d) {
+      return d.id;
+    })
+
+  simulation.force("links", link_force)
+
+  //draw lines for the links 
+  var link = svg.append("g")
+    .attr("class", "links")
+    .selectAll("line")
+    .data(links_data)
+    .enter().append("line")
+    .attr('stroke', 'white')
+    .attr("stroke-width", 2);
 }
 fetchData();
