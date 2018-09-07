dedbe61a48ce7fe53de795616bb1c67f3b02b097
SUEMIN JEONG <sueminee@SUEMINui-MacBook-Air.local>
2018-09-07 17:13:54 +0900

    flag 클릭하면 conut++

--- js/modal.js
+++ js/modal.js
@@ -24,44 +24,70 @@ var passingDataToModal = (index, starName) => {
         console.log('star를 클릭하면 db에 요청하는 data_____________: ', data)
 
         modalTitle = document.getElementById("headerH3");
-        modalTitle.innerHTML = `<h3>${starName}</h3>`;
+        modalTitle.innerHTML = `<h3>${data.starName}</h3>`;
     
         modalBody = document.getElementById("modal_body");
-        modalBody.innerHTML = `<p>이미지</p><p>메세지 : ${data.msg}</p><p>flag는 ${data.flag} 개</p><p>${data.createdAt}에 생성된 star</p>`;
+        modalBody.innerHTML = `
+            <p>이미지</p>
+            <p>메세지 : ${data.msg}</p>`;
+
+        modalFoot = document.getElementById("createdAt");
+        modalFoot.innerHTML = `<div class="box"><div>created at ${data.createdAt}</div><div><button id="flagButton" onclick="clickFlag(${data.id})">flag ${data.flag}</button></div></div>`;
     });
 }
+
+var clickFlag = (id, starName) => {
+    console.log(id)
+    fetch(`http://52.78.57.243:5000/flag/${id}`,{method: 'PUT'})
+    .then((res) => {
+        console.log(res)
+        passingDataToModal(id, starName);
+    })
+}
+
+var loadFile = function(event) {
+    var output = document.getElementById('output');
+    output.src = URL.createObjectURL(event.target.files[0]);
+};
 
 
 //=============================================================
 
 var makeInputModal = () => {
     modalTitle = document.getElementById("headerH3");
-    modalTitle.innerHTML = `<h3>Making new Star</h3>`;
+    modalTitle.innerHTML = `<h3>Making A New Star</h3>`;
 
     modalBody = document.getElementById("modal_body");
+
     modalBody.innerHTML = `
         <div class="container">
-            <form action="http://52.78.57.243:5000/star" method="POST">
-                <div>
+            <form id="formTag" target="_blank">
+                <div class="inputBox">
                     <label for="image"><b>image</b></label>
-                    <div><input type="file" id="image" name="image" accept=".jpg, .jpeg, .png" /> </div>
+                    <div>
+                        <input type="file" id="image" name="image" accept=".jpg, .jpeg, .png" onchange="loadFile(event)" />
+                        <img id="output"/>
+                    </div>
                 </div>
-                <div>
+                <div class="inputBox">
                     <label for="title"><b>title</b></label>
                     <div><input id="input_title" type="text" placeholder="제목을 입력해라" name="starName" onchange="inputChange(this.name, this.value)" required ></div>
                 </div>
-                <div>
+                <div class="inputBox">
                     <label for="msg"><b>message</b></label>
-                    <div><input id="input_msg" type="text" placeholder="메세지를 입력해라" name="msg"  onchange="inputChange(this.name, this.value)" required></div>
+                    <div><textarea id="input_msg" placeholder="메세지를 입력해라" name="msg"  onchange="inputChange(this.name, this.value)" required></textarea></div>
                 </div>
-                <div>
+                <div class="inputBox">
                     <label for="tag"><b>tag</b></label>
                     <div><input id="input_tag" type="text" placeholder="태그를 입력하세요. 여러개일 경우 comma(,)로 구분해주세요. (예: 마라샹궈,건대맛집,허씨네)" name="asterisms"  onchange="inputChange(this.name, this.value)"></div>
                 </div>
-                <button type="submit" onClick="submitNewStar()">post</button>
+                <div class="inputBox"><button type="submit" onClick="submitNewStar()">post</button></div>
             </form> 
         </div>   
         `;
+
+    modalFoot = document.getElementById("createdAt");
+    modalFoot.innerHTML = `<div class="box"></div>`;
 }
 //TODO:submit 함수 구현해야함. 
 //TODO:thumbnail 보이게 하기.
@@ -83,7 +109,7 @@ var asterismsssssssss = '태그1,태그2,태그3';
 
 
 inputChange = (name, value) => {
-    star[`${"name"}`] = `${value}`
+    star[`${name}`] = `${value}`
     console.log(star)
 }
 
@@ -94,17 +120,22 @@ var submitNewStar = () => {
         img : null
     }
     console.log(payload)
-    fetch('http://52.78.57.243:5000/star', {
-        method : 'POST',
-        headers: {
-            'Accept' : 'application/json, text/plain, */*',
-            'Access-Control-Allow-Origin': '*',
-            'Content-Type' : 'application/json'
-        },
-        body:JSON.stringify(payload)
-    })
-    .then((res) => res.json())
-    .then((data) => {
-        console.log('submitNewStar POST 후 response로 받는 data : ', data)
-    });
+
+    var formTag = document.getElementById("formTag");
+    console.log(formTag)
+
+//     fetch('http://52.78.57.243:5000/star', {
+    //     fetch("https:// jsonplaceholder.typicode.com/posts", {
+    //     method : 'POST',
+    //     headers: {
+    //         'Accept' : 'application/json, text/plain, */*',
+    //         'Access-Control-Allow-Origin': '*',
+    //         'Content-Type' : 'application/json'
+    //     },
+    //     body:JSON.stringify(payload)
+    // })
+    // .then((res) => res.json())
+    // .then((data) => {
+    //     console.log('submitNewStar POST 후 response로 받는 data : ', data)
+    // });
 }
