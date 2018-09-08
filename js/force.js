efa15b42292bedb3aa68f4a687a609d64c0723b5
이요한 <>
2018-09-08 11:20:27 +0900

    starimagefill

--- js/force.js
+++ js/force.js
@@ -26,21 +26,39 @@ d3.json('http://52.78.57.243:5000/asterism', (error, linkData) => {
     .scale(1000)  //여기를 조절해 원하는 크기의 우주를 생성합니다
     .translate([width / 2, height / 2])
     .clipAngle(config["clip"] ? 90 : null)
+  
+  
   var path = d3.geo.path()
+    .pointRadius(15)
     .projection(projection)
 
+    
   var force = d3.layout.force()
     .linkDistance(config["linkDistance"])
     .linkStrength(config["linkStrength"])
     .gravity(config["gravity"])
     .size([width, height])
     .charge(-config["charge"]);
+
+
   var svg = d3.select("#svgContainer").append("svg")
     .attr("width", width)
     .attr("height", height)
     .call(d3.behavior.drag()
       .origin(function () { var r = projection.rotate(); return { x: 2 * r[0], y: -2 * r[1] }; })
       .on("drag", function () { force.start(); var r = [d3.event.x / 2, -d3.event.y / 2, projection.rotate()[2]]; t0 = Date.now(); origin = r; projection.rotate(r); }))
+
+
+  svg.append('defs').append('pattern')
+     .attr('id','imgpattern')
+     .attr('x', 0)
+     .attr('y', 0)
+     .attr('width', 1)
+     .attr('height', 1)
+     .append('image')
+     .attr('width', 40)
+     .attr('height', 30)
+     .attr('xlink:href','http://viewers.heraldcorp.com/news/photo/201804/12640_7045_488.jpg')
 
   d3.json('http://52.78.57.243:5000/star', (err, nodeData) => {
     if (err) throw err;
@@ -61,27 +79,30 @@ d3.json('http://52.78.57.243:5000/asterism', (error, linkData) => {
 
     var colors = d3.scale.category10();
 
+
     var link = svg.selectAll("path.link")
       .data(links)
       .enter().append("path").attr("class", "link")
 
+      
     var node = svg.selectAll("path.node")
       .data(nodes)
       .enter().append("path").attr("class", "node")
       .style("stroke", function (d) { return '#000'; })
       .call(force.drag)
-      .style("fill", function (d, i) {
-        return colors(i);
-      })
+      .style("fill", 'url(#imgpattern)')
       .on("click", (e) => {
         passingDataToModal(e.index, e.starName);
         modal.style.display = "block";
       });
+
+
     force
       .nodes(nodes)
       .links(links)
       .on("tick", tick)
       .start();
+
     function tick() {
       node.attr("d", function (d) { var p = path({ "type": "Feature", "geometry": { "type": "Point", "coordinates": [d.x, d.y] } }); return p ? p : 'M 0 0' });
       link.attr("d", function (d) { var p = path({ "type": "Feature", "geometry": { "type": "LineString", "coordinates": [[d.source.x, d.source.y], [d.target.x, d.target.y]] } }); return p ? p : 'M 0 0' });
